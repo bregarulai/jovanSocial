416ec2ef5fb72425cbf450c9f2144eac10f4f186
bregarulai <bregarulai@gmail.com>
2021-11-04 18:25:21 -0400

    connected server to db

--- server/index.js
+++ server/index.js
@@ -1,6 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const dotenv = require("dotenv");
 
 const app = express();
+
+dotenv.config();
+
+mongoose.connect(
+  process.env.MONGO_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Connected to MongoDB.");
+  }
+);
 
 app.listen(8800, () => {
   console.log("Backend Server is running.");
