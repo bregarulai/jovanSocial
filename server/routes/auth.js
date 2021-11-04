c3cff267242dae2712b875c12b207bb5b141a167
bregarulai <bregarulai@gmail.com>
2021-11-04 19:26:33 -0400

    created register route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,25 @@
 const router = require("express").Router();
+const bcrypt = require("bcrypt");
 
-router.get("/", (req, res) => {
-  res.send("Using auth router");
+const User = require("../models/User");
+
+// REGISTER
+router.post("/register", async (req, res) => {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(req.body.password, salt);
+
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashPassword,
+    });
+    const user = await newUser.save();
+    const { password, ...info } = user._doc;
+    res.status(201).json(info);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
