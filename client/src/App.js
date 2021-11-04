8767768e32ce9415dea8e5699c18ba91c3ffb301
bregarulai <bregarulai@gmail.com>
2021-11-03 20:07:15 -0400

    created login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
+
 // import Home from "./pages/Home";
-import Profile from "./pages/Profile";
+//import Profile from "./pages/Profile";
+import Login from "./pages/Login";
 
 const App = () => {
   return (
     <div className="App">
-      <Profile />
+      <Login />
     </div>
   );
 };
---
+++ client/src/pages/Login/index.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+import "./login.css";
+
+const Login = () => {
+  return (
+    <div className="login">
+      <div className="loginWrapper">
+        <div className="loginLeft">
+          <h3 className="loginLogo">JovanSocial</h3>
+          <span className="loginDesc">
+            Connect with friends and the world around you on Jovan Social
+          </span>
+        </div>
+        <div className="loginRight">
+          <div className="loginBox">
+            <input placeholder="email" type="email" className="loginInput" />
+            <input
+              placeholder="password"
+              type="password"
+              className="loginInput"
+            />
+            <button className="loginButton">Log In</button>
+            <span className="loginForgot">Forgot password?</span>
+            <button className="loginRegisterButton">
+              Create a New Account
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
