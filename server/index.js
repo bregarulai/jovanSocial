78227856cbd6df7fe81c3d1b6da54e52284cf82f
bregarulai <bregarulai@gmail.com>
2021-11-04 18:44:54 -0400

    created auth and user router

--- server/index.js
+++ server/index.js
@@ -4,6 +4,9 @@ const morgan = require("morgan");
 const helmet = require("helmet");
 const dotenv = require("dotenv");
 
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+
 const app = express();
 
 dotenv.config();
@@ -15,6 +18,14 @@ mongoose.connect(
     console.log("Connected to MongoDB.");
   }
 );
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 
 app.listen(8800, () => {
   console.log("Backend Server is running.");
