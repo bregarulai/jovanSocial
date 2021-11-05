a620aa9206998507eb4cb53dc7458288095c984a
bregarulai <bregarulai@gmail.com>
2021-11-05 14:49:35 -0400

    created create post route

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const dotenv = require("dotenv");
 
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
 
 const app = express();
 
@@ -26,6 +27,7 @@ app.use(morgan("common"));
 
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
 
 app.listen(8800, () => {
   console.log("Backend Server is running.");
---
+++ server/routes/posts.js
@@ -0,0 +1,21 @@
+const router = require("express").Router();
+
+const Post = require("../models/Post");
+
+//CREATE A POST
+router.post("/", async (req, res) => {
+  const newPost = new Post(req.body);
+  try {
+    const savedPost = await newPost.save();
+    res.status(201).json(savedPost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+//UPDATE A POST
+//DELETE A POST
+//LIKE A POST
+//GET A POST
+//GET TIMELINE POSTS
+
+module.exports = router;
