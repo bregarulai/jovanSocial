f21704fc28d7bdd3ffca73da3067f39825127d97
bregarulai <bregarulai@gmail.com>
2021-11-05 15:08:05 -0400

    created update a post route

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -13,6 +13,19 @@ router.post("/", async (req, res) => {
   }
 });
 //UPDATE A POST
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.updateOne({ $set: req.body });
+      res.status(200).json("Your post has been updated!");
+    } else {
+      res.status(403).json("You can update only your posts!");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 //DELETE A POST
 //LIKE A POST
 //GET A POST
