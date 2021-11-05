a64ba4f59c7f3258af234ede634620f7155c7984
bregarulai <bregarulai@gmail.com>
2021-11-05 19:01:17 -0400

    created routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
-// import Profile from "./pages/Profile";
-// import Login from "./pages/Login";
-// import Register from "./pages/Register";
+import Profile from "./pages/Profile";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 const App = () => {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </Router>
   );
 };
 
