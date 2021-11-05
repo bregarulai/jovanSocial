718a062789105ff07bea6f14892631aa12428880
bregarulai <bregarulai@gmail.com>
2021-11-05 14:15:56 -0400

    created follow user route

--- server/routes/users.js
+++ server/routes/users.js
@@ -11,7 +11,7 @@ router.put("/:id", async (req, res) => {
         const salt = await bcrypt.genSalt(10);
         req.body.password = await bcrypt.hash(req.body.password, salt);
       } catch (err) {
-        return res.status(500).json(err);
+        res.status(500).json(err);
       }
     }
     try {
@@ -21,10 +21,10 @@ router.put("/:id", async (req, res) => {
       !user && res.status(404).json("User Not Found!");
       res.status(200).json("Account has been updated!");
     } catch (err) {
-      return res.status(500).json(err);
+      res.status(500).json(err);
     }
   } else {
-    return res.status(403).json("You can update only your account!");
+    res.status(403).json("You can update only your account!");
   }
 });
 //DELETE A USER
@@ -35,10 +35,10 @@ router.delete("/:id", async (req, res) => {
       !user && res.status(404).json("User Not Found!");
       res.status(200).json("Account has been deleted!");
     } catch (err) {
-      return res.status(500).json(err);
+      res.status(500).json(err);
     }
   } else {
-    return res.status(403).json("You can delete only your account!");
+    res.status(403).json("You can delete only your account!");
   }
 });
 //GET A USER
@@ -50,10 +50,29 @@ router.get("/:id", async (req, res) => {
     const { password, updatedAt, ...info } = user._doc;
     res.status(200).json(info);
   } catch (err) {
-    return res.status(500).json(err);
+    res.status(500).json(err);
   }
 });
 //FOLLOW A USER
+router.put("/:id/follow", async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+      if (!user.followers.includes(req.body.userId)) {
+        await user.updateOne({ $push: { followers: req.body.userId } });
+        await currentUser.updateOne({ $push: { followings: req.params.id } });
+        res.status(200).json("User has been followed!");
+      } else {
+        res.status(403).json("You already follow this user!");
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You can't follow yourself!");
+  }
+});
 //UNFOLLOW A USER
 
 module.exports = router;
