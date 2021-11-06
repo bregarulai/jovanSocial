1fbf278b17d6cca48ec1691bd37b3c08b57d0bcd
bregarulai <bregarulai@gmail.com>
2021-11-06 19:43:40 -0400

    created redirects when user is login

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,29 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useContext } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import { AuthContext } from "./context/AuthContext";
 
 const App = () => {
+  const { user } = useContext(AuthContext);
+  console.log("USER: ", user);
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route exact path="/" element={user ? <Home /> : <Register />} />
+        <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
+        <Route
+          path="/register"
+          element={user ? <Navigate to="/" /> : <Register />}
+        />
         <Route path="/profile/:username" element={<Profile />} />
       </Routes>
     </Router>
