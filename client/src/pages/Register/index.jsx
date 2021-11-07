69f1648aa2f95ce43e98cd9154d3923d1f7aeb3d
bregarulai <bregarulai@gmail.com>
2021-11-06 20:11:57 -0400

    implemented registration

--- client/src/pages/Register/index.jsx
+++ client/src/pages/Register/index.jsx
@@ -1,8 +1,34 @@
-import React from "react";
+import axios from "axios";
+import React, { useRef } from "react";
+import { useNavigate } from "react-router";
 
 import "./register.css";
 
 const Register = () => {
+  const email = useRef();
+  const username = useRef();
+  const password = useRef();
+  const confirmedPassword = useRef();
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (password.current.value !== confirmedPassword.current.value) {
+      confirmedPassword.current.setCustomValidity("Passwords don't match!");
+    } else {
+      const user = {
+        username: username.current.value,
+        password: password.current.value,
+        email: email.current.value,
+      };
+      try {
+        await axios.post("/auth/register", user);
+        navigate("/login");
+      } catch (err) {
+        console.log("ERROR: ", err);
+      }
+    }
+  };
   return (
     <div className="login">
       <div className="loginWrapper">
@@ -13,22 +39,46 @@ const Register = () => {
           </span>
         </div>
         <div className="loginRight">
-          <div className="loginBox">
-            <input placeholder="username" type="text" className="loginInput" />
-            <input placeholder="email" type="email" className="loginInput" />
+          <form className="loginBox" onSubmit={handleSubmit}>
+            <input
+              placeholder="username"
+              type="text"
+              className="loginInput"
+              required
+              ref={username}
+            />
+            <input
+              placeholder="email"
+              type="email"
+              className="loginInput"
+              required
+              ref={email}
+            />
             <input
               placeholder="password"
               type="password"
               className="loginInput"
+              required
+              ref={password}
+              minLength={6}
             />
             <input
               placeholder="confirm password"
               type="password"
               className="loginInput"
+              required
+              ref={confirmedPassword}
             />
-            <button className="loginButton">Sign Up</button>
-            <button className="loginRegisterButton">Log Into Account</button>
-          </div>
+            <button className="loginButton" type="submit">
+              Sign Up
+            </button>
+            <button
+              className="loginRegisterButton"
+              onClick={() => navigate("/login")}
+            >
+              Log Into Account
+            </button>
+          </form>
         </div>
       </div>
     </div>
